403012c75a3aaf545372116feefc804a30dda315
Adi Hai <adihai27@gmail.com>
2025-02-01 23:33:48 +0200

    second

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -6,7 +6,7 @@ export default function Message(props) {
 
     return (
         <div className='w-50 p-3 h3 bg-warning'>
-            <h2>Message: {props.txt}</h2>
+            <h2>Message  : {props.txt}</h2>
             <button onClick={()=>{
                 props.hideMessage()
             }}
